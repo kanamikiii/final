2d1437b4c78af3b9b40ebab568b29d009276313f
tung <doan.viet.tung@framgia.com>
2014-06-23 14:19:35 +0700

    basic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# fix title = Training when common_title is empty
+	def fix_title(common_title)
+		base_title = "Training"
+		if common_title.empty?
+			base_title 	
+		else
+			common_title
+		end
+	end
 end
